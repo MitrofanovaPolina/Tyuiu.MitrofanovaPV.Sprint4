5a641c433be0268f3d480689e927c2328ee407a0
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-11-12 04:02:32 +0500

    Task5

---
+++ Tyuiu.MitrofanovaPV.Sprint4.Task5.V25/Program.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint4.Task5.V25.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        DataService ds = new DataService();
+        Random rnd = new Random();
+        int rows = Convert.ToInt32(Console.ReadLine());
+        int cols = Convert.ToInt32(Console.ReadLine());
+        int[,] mtrx = new int[rows, cols];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                mtrx[i, j] = rnd.Next(-4, 3);
+            }
+        }
+        int res = ds.Calculate(mtrx);
+        Console.WriteLine(res);
+    }
+}

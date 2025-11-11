43f24dd4035c8492a02ec1ee0bb0ab8aaca56b27
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-11-11 22:58:09 +0500

    Add project files.

---
+++ Tyuiu.MitrofanovaPV.Sprint4.Task0.V21.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using tyuiu.cources.programming.interfaces;
+using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.MitrofanovaPV.Sprint4.Task0.V21.Lib
+{
+    public class DataService : ISprint4Task0V21
+    {
+        public int GetSumEvenArrEl(int[] array)
+        {
+            int sumev = 0;
+            for (int i = 0; i < array.Length ; i++)
+            {
+                if (array[i]%2 == 0)
+                {
+                    sumev += array[i];
+                }
+            }
+            return sumev;
+        }
+    }
+}
---
+++ Tyuiu.MitrofanovaPV.Sprint4.Task0.V21/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint4.Task0.V21.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int[] numsArray = { 8, 2, 4, 3, 8, 5, 6, 7, 9, 8 };
+        int res = ds.GetSumEvenArrEl(numsArray);
+        Console.WriteLine(res);
+    }
+}

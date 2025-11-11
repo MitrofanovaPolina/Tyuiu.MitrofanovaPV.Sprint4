94443f786aad75f84366ed8a7875aaad37d8aff7
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-11-12 00:06:04 +0500

    Task1

---
+++ Tyuiu.MitrofanovaPV.Sprint4.Task1.V18.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint4.Task1.V18.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint4.Task1.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 1, 2, };
+            int res = ds.Calculate(numsArray);
+            int wait = 1;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MitrofanovaPV.Sprint4.Task1.V18/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint4.Task1.V18.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int len;
+        len = Convert.ToInt32(Console.ReadLine());
+        int[]numsArray = new int[len];
+        for (int i = 0; i < len; i++)
+        {
+            numsArray[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        for (int i = 0;i < len ; i++)
+        {
+            Console.WriteLine(numsArray[i] + "\t");
+        }
+        int res = ds.Calculate(numsArray);
+        Console.WriteLine(res);
+    }
+}

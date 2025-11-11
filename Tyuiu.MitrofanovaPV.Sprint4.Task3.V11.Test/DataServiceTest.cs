23f9b42fa6eaca15447b2dc00aa275675ced0096
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-11-12 01:21:13 +0500

    Task3

---
+++ Tyuiu.MitrofanovaPV.Sprint4.Task3.V11.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint4.Task3.V11.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint4.Task3.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] mas2 = new int[3,3] { { 1,2,3}, {4,5,6}, {7,8,9} };
+            int res = ds.Calculate(mas2 );
+            int wait = 1;
+            Assert.AreEqual( wait, res );
+        }
+    }
+}
---
+++ Tyuiu.MitrofanovaPV.Sprint4.Task3.V11/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint4.Task3.V11.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] mtrx = new int[5, 5] { { 8, 6, 9, 4, 5 }, { 9, 4, 8, 5, 6 }, { 9, 7, 9, 8, 4 }, { 4, 6, 5, 7, 8 }, { 6, 6, 7, 6, 4 } };
+        DataService ds = new DataService();
+        for (int i = 0; i < mtrx.GetLength(0); i++)
+        {
+            for (int j = 0; j < mtrx.GetLength(1); j++)
+            {
+                Console.WriteLine($"{mtrx[i,j]} \t");
+            }
+        }
+        int res = ds.Calculate(mtrx);
+        Console.WriteLine(res);
+    }
+}
+

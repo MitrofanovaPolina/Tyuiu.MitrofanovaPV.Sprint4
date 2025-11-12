919b9a72984af2f2e1aa0882cce05fc4405b969c
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-11-12 06:58:51 +0500

    Task7

---
+++ Tyuiu.MitrofanovaPV.Sprint4.Task7.V13.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint4.Task7.V13.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint4.Task7.V13.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int rows = 3;
+            int cols = 3;
+            int[,] mtrx = new int[rows, cols];
+            string str = "159357246";
+            DataService ds = new DataService();
+            int res = ds.Calculate(rows,cols,str);
+            int wait = 3;
+            Assert.AreEqual(wait,res);
+        }
+    }
+}
---
+++ Tyuiu.MitrofanovaPV.Sprint4.Task7.V13/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint4.Task7.V13.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int rows = 3;
+        int cols = 3;
+        int[,] mtrx = new int[rows, cols];
+        string str = "159357246";
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int index = 0;
+        Console.WriteLine("\nМассив:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write($"{str[index]} \t");
+                index++;
+            }
+            Console.WriteLine();
+
+        }
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        int res = ds.Calculate(rows,cols,str);
+        Console.WriteLine("Количество четных =" + res);
+        Console.ReadKey();
+    }
+}

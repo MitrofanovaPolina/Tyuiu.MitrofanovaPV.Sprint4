940ec4c39b68cbc0beb4ca36e4d2c94e2ad677bf
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-11-12 05:02:53 +0500

    Task6

---
+++ Tyuiu.MitrofanovaPV.Sprint4.Task6.V26/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint4.Task6.V26.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        DataService ds = new DataService();
+        string[] data = { "Тюмень", "Тамбов", "Томск", "Омск", "Орёл" };
+        foreach (string item in data)
+        {
+            if (item.Length >5)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+    }
+}
